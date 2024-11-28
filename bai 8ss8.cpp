3d67172c8c9e3f1271140593eb40aadf90b2cb59
An256122006 <dinhtrongan2006@gmail.com>
2024-11-29 02:21:00 +0700

    Add files via upload

---
+++ bai 8ss8.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+int main()
+{
+	int b, sum=0;
+	printf ("moi ban nhap gai tri cua ma tran:");
+	scanf ("%d",&b); 
+	int a[b][b];
+	for (int i=0; i<b; i++)
+	{
+		for (int j=0; j<b; j++)
+		{
+			printf ("moi ban nhap hang thu %d gia tri thu %d:\n",i+1,j+1);
+			scanf ("%d",&a[i][j]);
+		}
+    }
+	for (int i=0; i<b; i++)
+	{
+		sum = sum+a[i][b-1-i]; 
+    }
+    for (int i=0; i<b; i++)
+	{
+		printf ("phan tu thu %d cua duong cheo phu là :%d\n",i+1, a[i][b-1-i]); 
+    }
+    printf ("tong duong cheo:%d",sum);
+	return 0;
+}
